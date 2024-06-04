d2aedfecab102dc6280816b0f09f516b5c5e6c91
Nora S. Klasen <107119061+Nalisala@users.noreply.github.com>
2024-06-04 15:11:14 +0200

    Update Virsorter2.sh

--- Scripts/Virsorter2.sh
+++ Scripts/Virsorter2.sh
@@ -10,3 +10,12 @@ conda activate vs2
 cd ../vsc45617/5.assembly/ns_goes_viral/trim2 for sample in `cat samplelistmega`;                                  
 do                                                              
 virsorter run --keep-original-seq -w /data/gent/vo/001/gvo00125/vsc45617/7.identification/ns_goes_viral/virsorter/trim2/${sample}.out -i /data/gent/vo/001/gvo00125/vsc45617/5.assembly/ns_goes_viral/trim2/${sample}/final.contigs.fa --include-groups dsDNAphage,NCLDV,RNA,ssDNA,lavidaviridae --min-length 1500 --min-score 0.5 -j 20 all; done
+
+#After the CheckV run virsorter again 
+#combine provirus.fna and virus.fna from checkv output
+ 
+		cat checkv/proviruses.fna checkv/viruses.fna > checkv/combined.fna
+  
+#Adding following parameter to virsorter command:
+
+	--seqname-suffix-off --viral-gene-enrich-off --prep-for-dramv
