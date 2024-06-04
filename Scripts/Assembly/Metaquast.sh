a13272d75ef7e1aaa53a7561f908ce58430733a8
Nora S. Klasen <107119061+Nalisala@users.noreply.github.com>
2024-06-04 14:22:36 +0200

    Create Metaquast.sh

---
+++ Scripts/Assembly/Metaquast.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH --partition=HMEM1    # default "normal", if not specified
+#SBATCH --time=3-00:00:00  # run time in days-hh:mm:ss
+#SBATCH --nodes=1    # require 3 nodes
+#SBATCH --ntasks-per-node=80  # (by default, "ntasks"="cpus")
+#SBATCH --mem-per-cpu=8000  # MB RAM per CPU core (default 4 GB/core)
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
+source ~/.bashrc.conda3
+conda activate metaquast
+
+cd ~/Projects/NSV_1/working/metaquast/prinseq++/megahit/metaquast
+
+metaquast.py ../*/*megahit.fa -o ./metaquast_together --max-ref-number 0
+
+conda deactivate
+
+#Compile all of the reports in one folder
+#for sample in `cat ~/Projects/NSV_1/data/sample_list`
+#do
+#mkdir metaquast_reports
+#cp ${sample}/report.html ./metaquast_reports/${sample}_metaquast_report.html
+#cp ${sample}/report.tsv ./metaquast_reports/${sample}_metaquast_report.tsv
+#done
